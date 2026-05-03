2e0631952952812d2040e6aa1feaffc5dae53692
Joseph Zambrano <jz09022005@gmail.com>
2026-05-03 11:23:42 -0500

    Creación básica de index y controlador producto

---
+++ controllers/ProductoController.php
@@ -0,0 +1,41 @@
+<?php
+require_once "../config/database.php";
+require_once "../models/Producto.php";
+
+class ProductoController {
+
+    private $db;
+    private $producto;
+
+    public function __construct() {
+        $database = new Database();
+        $this->db = $database->getConnection();
+        $this->producto = new Producto($this->db);
+    }
+
+    // Crear producto con validaciones
+    public function crear($data) {
+
+        if (empty($data['nombre'])) {
+            return "Nombre obligatorio";
+        }
+
+        if ($data['stock'] < 0) {
+            return "Stock no puede ser negativo";
+        }
+
+        if ($data['precio'] <= 0) {
+            return "Precio debe ser mayor a 0";
+        }
+
+        $this->producto->nombre = $data['nombre'];
+        $this->producto->stock = $data['stock'];
+        $this->producto->precio = $data['precio'];
+
+        return $this->producto->crear();
+    }
+
+    public function listar() {
+        return $this->producto->listar();
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,36 @@
+<?php
+require_once "../controllers/ProductoController.php";
+
+$controller = new ProductoController();
+$productos = $controller->listar();
+?>
+
+<h1>Inventario de Productos</h1>
+
+<form method="POST" action="productos.php">
+    <input type="text" name="nombre" placeholder="Nombre">
+    <input type="number" name="stock" placeholder="Stock">
+    <input type="number" name="precio" placeholder="Precio">
+    <button type="submit">Guardar</button>
+</form>
+
+<hr>
+
+<table border="1">
+<tr>
+    <th>ID</th>
+    <th>Nombre</th>
+    <th>Stock</th>
+    <th>Precio</th>
+</tr>
+
+<?php while($row = $productos->fetch(PDO::FETCH_ASSOC)) { ?>
+<tr>
+    <td><?= $row['id'] ?></td>
+    <td><?= $row['nombre'] ?></td>
+    <td><?= $row['stock'] ?></td>
+    <td><?= $row['precio'] ?></td>
+</tr>
+<?php } ?>
+
+</table>
